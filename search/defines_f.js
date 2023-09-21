2a350269306a5d3f1c9795fe88a5ef0bf92ee7a6
Gallant-Char-25 <Gallant-Char-25@users.noreply.github.com>
2023-09-21 14:18:23 +0000

    deploy: 6e385d6dca39e3852b905c7cc682fd31ef22a8d4

---
+++ search/defines_f.js
@@ -0,0 +1,6 @@
+var searchData=
+[
+  ['togcol_0',['TOGCOL',['../gamemode__defense_8cpp.html#a71bf5c7cbab4caf15644e39ef09ed2a4',1,'gamemode_defense.cpp']]],
+  ['translate_5fmarker_1',['translate_marker',['../translations_8h.html#a0b3ab525ec6b450b2487e7644255fa7c',1,'translations.h']]],
+  ['translate_5fmarker_5fcontext_2',['translate_marker_context',['../translations_8h.html#a9e39a96618079002323c6e554271d3e2',1,'translations.h']]]
+];
---
+++ search/namespaces_8.js
@@ -0,0 +1,4 @@
+var searchData=
+[
+  ['json_5fexport_0',['json_export',['../namespacejson__export.html',1,'']]]
+];
